16097623ea5f0bc0e95f26f9323342c8c1386049
Daksh Singla <157093114+DakshSingla@users.noreply.github.com>
2025-10-28 10:03:16 +0530

    0104-maximum-depth-of-binary-tree .java Easy

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -15,7 +15,29 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if(root==null) return 0;
-        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int level = 0;
+        if (root == null) return 0;
+        while(true){
+            int countlevel = q.size();
+            if(countlevel== 0){
+                return level;
+            }
+            while(countlevel>0){
+                TreeNode ele = q.poll();
+                if(ele.left!=null){
+                    q.add(ele.left);
+                }
+                if(ele.right!=null){
+                    q.add(ele.right);
+                }
+                countlevel--;
+            }
+            level++;
+        }
+
+        // if(root==null) return 0;
+        // return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
     }
 }
