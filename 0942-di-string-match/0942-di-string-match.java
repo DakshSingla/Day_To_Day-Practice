46d02f7851e4ce0a5b9105633a3ec51f2b320c48
Daksh Singla <157093114+DakshSingla@users.noreply.github.com>
2025-11-16 23:36:11 +0530

    0942-di-string-match .java Easy

---
+++ 0942-di-string-match/0942-di-string-match.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int n = s.length();
+        int a = 0;
+        int b = n;
+        int[] arr = new int[n+1];
+        for(int i = 0;i<n;i++){
+            char c = s.charAt(i);
+            if(c=='I'){
+                arr[i] = a;
+                a++;
+            }
+            if(c=='D'){
+                arr[i]=b;
+                b--;
+            }
+        }
+        arr[n] = a;
+        return arr;
+    }
+}
