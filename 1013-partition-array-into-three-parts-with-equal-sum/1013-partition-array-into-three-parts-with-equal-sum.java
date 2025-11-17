258c5d66bddc0c52b2edcda392f7e151ac36857b
Daksh Singla <157093114+DakshSingla@users.noreply.github.com>
2025-11-17 18:13:03 +0530

    1013-partition-array-into-three-parts-with-equal-sum .java Easy

---
+++ 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canThreePartsEqualSum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i<arr.length;i++){
+            sum += arr[i];
+        }
+        if(sum%3!= 0) return false;
+        int cur = 0;
+        int count =0;
+        int p = sum/3;
+        for(int i = 0;i<arr.length;i++){
+            cur += arr[i];
+            if(cur == p){
+                count++;
+                cur = 0;
+            }
+        }
+        return count>=3;
+    }
+}
