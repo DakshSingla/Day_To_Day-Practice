70fac6836e92a6834e13cc4203da3a5f37644374
Daksh Singla <157093114+DakshSingla@users.noreply.github.com>
2025-11-28 23:48:07 +0530

    0347-top-k-frequent-elements .java Medium

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i= 0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],1)+1);
+        }
+        PriorityQueue<Map.Entry<Integer, Integer>> pq =new PriorityQueue<>((a, b) -> a.getValue()-b.getValue());
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            pq.add(entry);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        int[] ans = new int[k];
+        int i = k - 1;
+
+        while (!pq.isEmpty()) {
+            ans[i--] = pq.poll().getKey();
+        }
+
+        return ans;
+    }
+}
