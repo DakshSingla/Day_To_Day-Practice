63e321223ca1247c41a19e948c697fb48a2c855f
Daksh Singla <157093114+DakshSingla@users.noreply.github.com>
2025-12-14 22:21:07 +0530

    0414-third-maximum-number .java Easy

---
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int n : nums) {
+            set.add(n);
+        }
+
+        Integer[] arr = set.toArray(new Integer[0]);
+        Arrays.sort(arr);
+
+        int n = arr.length;
+        if (n < 3) {
+            return arr[n - 1];
+        }
+        return arr[n - 3];
+    }
+}
