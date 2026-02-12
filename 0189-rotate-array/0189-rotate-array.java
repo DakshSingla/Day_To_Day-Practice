979bd1bed2223aeee0df1a2c32c3ec3ff8b1c023
Daksh Singla <157093114+DakshSingla@users.noreply.github.com>
2026-02-12 19:21:37 +0530

    0189-rotate-array 02-12-2026 Medium .java

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,26 +1,16 @@
 class Solution {
-    public void reverse(int nums[], int i, int j){
-        while(i<j){
-            int temp = nums[i];
-            nums[i]=nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
-        }
-    }
     public void rotate(int[] nums, int k) {
-        int n =nums.length;
-        k=k%n;
-        reverse(nums,0,n-1);
-        reverse(nums,0,k-1);
-        reverse(nums,k,n-1);
-    }
-    public static void main(String[] args){
-        Scanner obj = new Scanner(System.in);
-        int n = obj.nextInt();
-        int nums[]=new int[n];
-        for(int i=0;i<n;i++){
-            nums[i]=obj.nextInt();
+        k=k%nums.length;
+         while (k > 0) {
+            int t = nums[nums.length - 1];
+            for (int j = nums.length - 1; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[0] = t;
+            k--;
+        }
+        for(int i = 0;i<nums.length;i++){
+            System.out.print(nums[i]+" ");
         }
     }
 }
