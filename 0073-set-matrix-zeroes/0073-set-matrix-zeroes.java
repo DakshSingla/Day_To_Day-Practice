cb9a82a0e6a30ca8e20576dda0bbdd7445a0a5d7
Daksh Singla <157093114+DakshSingla@users.noreply.github.com>
2026-02-18 17:38:33 +0530

    0073-set-matrix-zeroes 02-18-2026 Medium .java

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        boolean f = false,s=false;
+        for(int i = 0;i<rows;i++){
+            for(int j = 0;j<cols;j++){
+                if(matrix[i][j]==0){
+                    if(i==0) f=true;
+                    if(j==0) s=true;
+                    matrix[0][j]=0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for(int i = 1;i<rows;i++){
+            for(int j = 1;j<cols;j++){
+                if(matrix[0][j]==0||matrix[i][0]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(f){
+            for(int i = 0;i<cols;i++){
+                 matrix[0][i]=0;
+            }
+        }
+        if(s){
+            for(int i = 0;i<rows;i++){
+                 matrix[i][0]=0;
+            }
+        }
+
+    }
+}
