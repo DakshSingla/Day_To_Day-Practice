22081961c29fb0af7418f85f17b8894b80850310
Daksh Singla <157093114+DakshSingla@users.noreply.github.com>
2026-03-25 15:26:49 +0530

    1903-largest-odd-number-in-string 03-25-2026 Easy .java

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
@@ -1,11 +1,43 @@
 class Solution {
     public String largestOddNumber(String num) {
         for (int i = num.length() - 1; i >= 0; i--) {
-            int digit = num.charAt(i) - '0';
-            if (digit % 2 != 0) {
+            if ((num.charAt(i) - '0') % 2 != 0) {
                 return num.substring(0, i + 1);
             }
         }
         return "";
+    //     String ans = "";
+    //     for( int i = 0;i<num.length();i++){
+    //         String temp =""+ num.charAt(i);
+    //         if((num.charAt(i) - '0')%2 != 0){
+    //                if(Integer.parseInt(temp) > Integer.parseInt(ans){
+    //                     ans = temp;
+    //                 }
+    //             }
+    //         for(int j = i+1;j<num.length();j++){
+    //             temp = temp + num.charAt(j);
+    //             if((num.charAt(j) - '0')%2 != 0){
+    //                if(Integer.parseInt(temp) > Integer.parseInt(ans)){
+    //                     ans = temp;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return ans;
+
+    //    String ans = ""; 
+    //    for( int i = 0;i<num.length();i++)
+    //    {
+    //      String temp = "";
+    //       temp = num[i]-'a';
+    //        for(int j = i+1;j<num.length();j++)
+    //        {
+    //          if((num[j]-'a')%2 != 0)
+    //          {
+    //              ans = Math.max(ans,temp);
+    //          } 
+    //          temp = temp + num.charAt(j);
+    //      }
+    //      } return ans;
     }
 }
