9009699eea780235ddee611aaa24513cab9b0e59
Daksh Singla <157093114+DakshSingla@users.noreply.github.com>
2026-04-06 22:57:20 +0530

    0704-binary-search 04-06-2026 Easy .java

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,17 +1,17 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int l = 0,r = nums.length-1;
-        while(l<=r){
-            int mid = l + (r - l) / 2;
-            if(nums[mid]==target){
+        int i = 0;
+        int j = nums.length -1;
+        while(i<=j){
+            int mid = i+(j-i)/2;
+            if(nums[mid] == target){
                 return mid;
             }
-            else if(nums[mid]<target){
-                l = mid+1;
+            else if(nums[mid] > target){
+                j = mid-1;
             }
-            else{
-                r = mid-1;
-            }
-        }return -1;
+            else i = mid+1;
+        }
+        return -1;
     }
 }
