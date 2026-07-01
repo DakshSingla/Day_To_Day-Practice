31cd2422e7668f99ae4a0badd66933223c5d128c
Daksh Singla <157093114+DakshSingla@users.noreply.github.com>
2026-07-02 00:51:36 +0530

    0040-combination-sum-ii 07-02-2026 Medium .java

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -2,21 +2,20 @@ class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> ans = new ArrayList<>();
-        check(0,candidates,target,new ArrayList<>(),ans);
+        solve(ans,new ArrayList<>(),candidates,target,0);
         return ans;
     }
-    public void check(int idx, int[] arr, int target, List<Integer> ds, List<List<Integer>>ans){
-        if(target==0){
-            ans.add(new ArrayList<>(ds));
+    public void solve(List<List<Integer>> ans,List<Integer> sub, int[] arr, int t,int idx){
+        if(t == 0){
+            ans.add(new ArrayList<>(sub));
             return;
         }
-        for(int i = idx; i<arr.length;i++){
-            if(i>idx && arr[i] == arr[i-1]) continue;
-            if(arr[i] > target) break;
-            ds.add(arr[i]);
-            check(i+1, arr, target - arr[i], ds, ans);
-            ds.remove(ds.size()-1);
+        for(int i = idx;i<arr.length;i++){
+            if(i>idx&&arr[i] == arr[i-1]) continue;
+            if(arr[i] > t) break;
+            sub.add(arr[i]);
+            solve(ans,sub,arr,t-arr[i],i+1);
+            sub.remove(sub.size()-1);
         }
-        
     }
 }
