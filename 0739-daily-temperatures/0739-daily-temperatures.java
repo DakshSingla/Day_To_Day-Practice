a741baef4c77dfc989c6c8b678c02d797f513c69
Daksh Singla <157093114+DakshSingla@users.noreply.github.com>
2026-07-09 20:18:52 +0530

    0739-daily-temperatures 07-09-2026 Medium .java

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,16 +1,13 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> st = new Stack<>();
         int n = temperatures.length;
         int[] ans = new int[n];
-        Stack<Integer> st = new Stack();
-        // int count = 0;
-        for(int i=0;i<n;i++){
-            while(!st.isEmpty() && temperatures[st.peek()]<temperatures[i]){
-                int a = st.pop();
-                ans[a] = i - a;
+        for(int i = 0;i<n;i++){
+            while(!st.isEmpty() && temperatures[st.peek()]< temperatures[i]){
+                ans[st.peek()] = i-st.pop();
             }
             st.push(i);
-        
         }
         return ans;
     }
