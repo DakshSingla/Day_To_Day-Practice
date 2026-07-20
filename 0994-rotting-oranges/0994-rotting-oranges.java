7c1d15f4d4413dcbaa284d5a3fc0ee5528f75d79
Daksh Singla <157093114+DakshSingla@users.noreply.github.com>
2026-07-20 20:11:03 +0530

    0994-rotting-oranges 07-20-2026 Medium .java

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,41 +1,48 @@
 class Solution {
+    public class pair{
+        int r;
+        int c;
+        pair(int r , int c){
+            this.r = r;
+            this.c = c;
+        }
+    }
     public int orangesRotting(int[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
         int pend = 0;
+        Queue<pair> q = new LinkedList<>();
         for(int i = 0;i<rows;i++){
             for(int j = 0;j<cols;j++){
-                if(grid[i][j]== 1){
+                if(grid[i][j] == 1){
                     pend++;
                 }
-                else if (grid[i][j] == 2){
-                    q.offer(new int[]{i,j});
+                if(grid[i][j]==2){
+                    q.add(new pair(i,j));
                 }
             }
         }
         if(pend == 0) return 0;
-        int[][] dir = {{-1,0},{0,-1},{1,0},{0,1}};
+        int[][] dir = new int[][]{
+            {-1,0},{1,0},{0,-1},{0,1}
+        };
         int ans=-1;
         while(!q.isEmpty()){
             int size = q.size();
             for(int i = 0;i<size;i++){
-                int[] cur = q.poll();
-                int r = cur[0];
-                int c = cur[1];
-                for(int[] d:dir){
-                    int newrow = r+d[0];
-                    int newcol = c+d[1];
-                    if(rows>newrow && newrow >=0 && cols>newcol && newcol>=0 && 
-                    grid[newrow][newcol] == 1){
-                        grid[newrow][newcol] = 2;
-                        pend--;
-                        q.offer(new int[]{newrow,newcol});
-                    }
+            pair cur = q.poll();
+            for(int[] d:dir){
+                int nr = cur.r+d[0];
+                int nc = cur.c+d[1];
+                if(nr>=0 && nr <rows && nc >=0&& nc<cols && grid[nr][nc]==1){
+                    grid[nr][nc] = 2;
+                    q.add(new pair(nr,nc));
+                    pend--;
                 }
-            }
+            }}
             ans++;
         }
-        return (pend == 0) ? ans:-1;
+        return (pend == 0)? ans:-1;
+
     }
 }
