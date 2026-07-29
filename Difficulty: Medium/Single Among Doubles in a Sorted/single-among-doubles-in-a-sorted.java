4b839510ff1a3e4eab24551511a1a55ad388b918
Daksh Singla <157093114+DakshSingla@users.noreply.github.com>
2026-07-29 18:42:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Single Among Doubles in a Sorted/single-among-doubles-in-a-sorted.java
@@ -0,0 +1,15 @@
+class Solution {
+    int single(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i:arr){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(Map.Entry<Integer,Integer> e:map.entrySet()){
+            if(e.getValue() == 1){
+                return e.getKey();
+            }
+        }
+        return -1;
+        
+    }
+}
